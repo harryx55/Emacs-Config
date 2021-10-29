8319baa723acfa01ea8de4e69f3dff4fa6a1e1f4
harryx55 <54209982+harryx55@users.noreply.github.com>
2021-10-29 09:43:25 +0200

    init file

--- init.el
+++ init.el
@@ -1,204 +1,39 @@
-;; newly added (auto-complete)
-;; c++ configuration
+(message "loading init file...")
+(unless (>= emacs-major-version 24)
+  (error "Requires GNU Emacs 24 or later"))
+
 (require 'package)
-(add-to-list 'package-archive '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(use-package try :ensure t)
-(use-package which-key :ensure t :config (which-key-mode))
-
-;;; c++ auto-complete
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-;;; added flycheck for syntax checking by reporting errors and warnings
-(use-package auto-complete)
-(use-package flycheck
-  :ensure t
-  :init
-  (global-flycheck-mode t))
-
-;;; modern c++ font package
-(use-package modern-cpp-font-lock
-  :ensure t)
-
-;;; magit integration
-(use-package magit
-  :ensure t
-  :init
-  (progn
-    (bind-key "C-x g", 'magit-status)))
-
-;;; compiling and running c++ code
-(defun code-compile ()
-  (interactive)
-  (unless (file-exists-p- "Makefile")
-    (set (make-local-variable 'compile-command)
-	 (let ((file (file-name-nondirectory buffer-file-name)))
-	   (format "%s -o %s %s"
-		   (if (equal (file-name-extension file) "cpp" "c" "cxx") "clang" "cl" "g++" "gcc")
-		   (file-name-sans-extension file)
-		   file)))
-    (compile compile-command)))
-(global-set-key [f5] 'code-compile)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
 
-;; keep the installed packages in .emacs.d
-;; (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
-;; (package-initialize)
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;; update the package metadata is the local cache is missing
-(unless package-archive-contents
-  (package-refresh-s package
-  (add-to-list 'company-backends 'company-c-headers)
+(add-to-list 'load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/packages")
+(add-to-list 'load-path "~/.emacs.d/elpa")
 
-;; lsp-mode and company-mode setup
-;; (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-;; (package-initialize)
+;; warn user when opening files larger then 10MBs
+(setq large-file-warning-threshold 10000000)
 
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
-    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+;; disable bell ring
+(setq ring-bell-function 'ignore)
 
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
+;; disable menu
+(menu-bar-mode -1)
 
-;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
-(helm-mode)
-(require 'helm-xref)
-(define-key global-map [remap find-file] #'helm-find-files)
-(define-key global-map [remap execute-extended-command] #'helm-M-x)
-(define-key global-map [remap switch-to-buffer] #'helm-mini)
+;; disable scroll bar
+(toggle-scroll-bar -1)
 
-(which-key-mode)
-(add-hook 'c-mode-hook 'lsp)
-(add-hook 'c++-mode-hook 'lsp)
-
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      treemacs-space-between-root-nodes nil
-      company-idle-delay 0.0
-      company-minimum-prefix-length 1
-      lsp-idle-delay 0.1)  ;; clangd is fast
-
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (require 'dap-cpptools)
-  (yas-global-mode))
-
-;;; Auto-complete with ycmd stuff
-
-;; Snippets
-(use-package yasnippet
-  :ensure t
-  :diminish yas-minor-mode
-  :init (yas-global-mode t))
-
-;; Autocomplete
-(use-package company
-  :defer 10
-  :diminish company-mode
-  :bind (:map company-active-map
-              ("M-j" . company-select-next)
-              ("M-k" . company-select-previous))
-  :preface
-  ;; enable yasnippet everywhere
-  (defvar company-mode/enable-yas t
-    "Enable yasnippet for all backends.")
-  (defun company-mode/backend-with-yas (backend)
-    (if (or 
-         (not company-mode/enable-yas) 
-         (and (listp backend) (member 'company-yasnippet backend)))
-        backend
-      (append (if (consp backend) backend (list backend))
-              '(:with company-yasnippet))))
-
-  :init (global-company-mode t)
-  :config
-  ;; no delay no autocomplete
-  (validate-setq
-   company-idle-delay 0
-   company-minimum-prefix-length 2
-   company-tooltip-limit 20)
-
-  (validate-setq company-backends 
-                 (mapcar #'company-mode/backend-with-yas company-backends)))
-
-;; Code-comprehension server
-(use-package ycmd
-  :ensure t
-  :init (add-hook 'c++-mode-hook #'ycmd-mode)
-  :config
-  (set-variable 'ycmd-server-command '("python2" "~/.emacs.d/elpa/ycmd-20190416.807/ycmd.el"))
-  (set-variable 'ycmd-global-config (expand-file-name "~/.emacs.d/elpa/ycmd-20190416.807/ycm_conf.py"))
-
-  (set-variable 'ycmd-extra-conf-whitelist '("~/Repos/*"))
-
-  (use-package company-ycmd
-    :ensure t
-    :init (company-ycmd-setup)
-    :config (add-to-list 'company-backends (company-mode/backend-with-yas 'company-ycmd))))
+;; disable tool bar
+(tool-bar-mode -1)
 
 
-;;; emacs-c-ide-demo stuff
-;; comment start
-(require 'setup-ggtags)
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (when (derived-mode-p 'c-mode 'c++mode 'java-mode 'asm-mode)
-	      (ggtags-mode 1))))
-
-(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
-(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
-(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
-(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
-(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
-(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
-
-(define-key ggtags-mode-map (kbd "M-, ") 'pop-tag-mark)
-
-(setq
- helm-gtags-ignore-case t
- helm-gtags-auto-update t
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t
- helm-gtags-prefix-key "\C-cg"
- helm-gtags-suggested-key-mapping t
- )
-
-(require 'helm-gtags)
-;; Enable helm-gtags-mode
-(add-hook 'dired-mode-hook 'helm-gtags-mode)
-(add-hook 'eshell-mode-hook 'helm-gtags-mode)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-
-(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
 
 
-(require 'setup-helm)
-;; comment end
